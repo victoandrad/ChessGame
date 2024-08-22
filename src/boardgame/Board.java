015ab25fda9f08d5d2d61b4d18c87aee0c7b5be8
Victor Andrade <victor64gaming@gmail.com>
2024-08-22 17:22:58 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -11,6 +11,7 @@ public class Board {
     // BUILDERS
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) throw new BoardException("Board must have at least 1 row and 1 column");
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
@@ -19,6 +20,7 @@ public class Board {
     // METHODS
 
     public Piece getPieceAt(int row, int column) {
+        if (!positionExists(row, column)) throw new BoardException("Position not found");
         return pieces[row][column];
     }
 
@@ -27,8 +29,22 @@ public class Board {
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) throw new BoardException("There is already a piece in this position");
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    public boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) throw new BoardException("Position not found");
+        return getPieceAt(position) != null;
     }
 
     // GETTERS
@@ -39,15 +55,5 @@ public class Board {
 
     public int getColumns() {
         return columns;
-    }
-
-    // SETTERS
-
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    public void setColumns(int columns) {
-        this.columns = columns;
     }
 }
